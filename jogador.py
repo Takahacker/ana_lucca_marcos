e07b59325557ab0cd579e3c542700467c37f7010
Ana Clara <anacmm2@al.insper.edu.br>
2023-05-20 12:43:41 -0300

    jogador

---
+++ jogador.py
@@ -0,0 +1,127 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+# ----- Gera tela principal
+WIDTH = 600
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Crossy Ocean')
+
+# ----- Inicia assets
+largura = 50
+altura = 38
+larg_tub = 130
+alt_tub = 90
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('imagens/fundo_mario.jpg').convert()
+
+
+
+peixe1 = pygame.image.load('imagens/peixe1.png').convert_alpha()
+peixe1_small = pygame.transform.scale(peixe1, (largura, altura))
+
+tubarao = pygame.image.load('imagens/shark03.png').convert_alpha()
+tubarao_grande = pygame.transform.scale(tubarao, (larg_tub, alt_tub))
+
+player = pygame.image.load('imagens/jogador2.png').convert_alpha()
+player_image = pygame.transform.scale(player, (largura, altura))
+
+# ----- Inicia estruturas de dados
+
+game = True
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups, imagens):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = imagens['jogador2']
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.groups = groups
+        self.assets = imagens
+
+    def update(self):
+        # Atualização da posição da nave
+        self.rect.x += self.speedx
+
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+class Peixe(pygame.sprite.Sprite):
+    def __init__(self, imgagens):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = imgagens
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = random.randint(-100, -altura)
+        self.speedx = 2
+        self.speedy = 0
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o peixe passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = 0
+            self.rect.y = random.randint(-100, HEIGHT - altura)
+            self.speedx = random.randint(2, 4)
+            self.speedy = 0
+
+
+all_peixes = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+# Criando o jogador
+player = Player(player_image)
+all_sprites.add(player)
+
+for i in range(6):
+    peixe1 = Peixe(peixe1_small)
+    all_peixes.add(peixe1)
+
+for i in range(4):
+    tubarao = Peixe(tubarao_grande)
+    all_peixes.add(tubarao)
+
+clock = pygame.time.Clock()
+FPS = 30
+
+# ===== Loop principal =====
+while game:
+    clock.tick(FPS)
+    
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição dos peixes
+    for peixe in all_peixes:
+        peixe.update()
+
+    # ----- Gera saídas
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    # Desenhando meteoros
+    all_peixes.draw(window)
+
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
