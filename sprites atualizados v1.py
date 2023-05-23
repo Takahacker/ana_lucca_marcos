850ff701a5aae16fb7a9273e5fd5c3eddc56d7ac
marcoss0ft <marcosarpc@al.insper.edu.br>
2023-05-23 09:08:48 -0300

    sprites atualizados e framerate aumentado

---
+++ sprites atualizados v1.py
@@ -0,0 +1,244 @@
+import pygame
+import random
+
+    
+# Inicialização do Pygame
+pygame.init()
+
+# Configurações da janela em Pygame
+largura = 800
+altura = 600
+janela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Botão de Reprodução")
+
+# Cores em Pygame
+BRANCO = (255, 255, 255)
+VERDE = (0, 255, 0)
+
+# Posição e dimensões do botão em Pygame
+botao_largura = 100
+botao_altura = 100
+score1 = 0
+score2 = 0
+botao_posicao_x = (largura - botao_largura) // 2
+botao_posicao_y = (altura - botao_altura) // 2
+
+# Variável para verificar se o botão está pressionado em Pygame
+botao_clicado = False
+
+# Loop principal do jogo em Pygame
+executando = True
+while executando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            executando = False
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if botao_posicao_x <= pygame.mouse.get_pos()[0] <= botao_posicao_x + botao_largura \
+                    and botao_posicao_y <= pygame.mouse.get_pos()[1] <= botao_posicao_y + botao_altura:
+                botao_clicado = True
+
+    janela.fill(BRANCO)
+
+    # Desenha o botão em Pygame
+    pygame.draw.rect(janela, VERDE, (botao_posicao_x, botao_posicao_y, botao_largura, botao_altura))
+    if not botao_clicado:
+        pygame.draw.polygon(janela, BRANCO, [(botao_posicao_x + 30, botao_posicao_y + 20),
+                                                  (botao_posicao_x + 30, botao_posicao_y + 80),
+                                                  (botao_posicao_x + 80, botao_posicao_y + 50)])
+    else:
+
+        pygame.init()
+
+        # ----- Gera tela principal
+        WIDTH = 600
+        HEIGHT = 600
+        window = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Crossy Ocean')
+
+        # ----- Inicia assets
+        largura = 60
+        altura = 60
+        larg_tub = 130
+        alt_tub = 90
+        font = pygame.font.SysFont('imagens/Fontes/retro_mario/RetroMario-Regular.otf', 48)
+        background = pygame.image.load('imagens/fundo_mario.jpg').convert()
+
+        agua_viva = pygame.image.load('imagens/AGUAVIVA.png').convert_alpha()
+        agua_viva_small = pygame.transform.scale(agua_viva, (largura, altura))
+
+        tubarao = pygame.image.load('imagens/holandes.png').convert_alpha()
+        tubarao_grande = pygame.transform.scale(tubarao, (larg_tub, alt_tub))
+
+        player_image1 = pygame.image.load('imagens/bob_esponja_direita.png').convert_alpha()
+        player_image1 = pygame.transform.scale(player_image1, (largura, altura))
+
+        player_image2 = pygame.image.load('imagens/patrick.png').convert_alpha()
+        player_image2 = pygame.transform.scale(player_image2, (largura, altura))
+
+        # ----- Inicia estruturas de dados
+        game = True
+        score = 0
+
+        class Player(pygame.sprite.Sprite):
+            def __init__(self, imagens, keys):
+                # Construtor da classe mãe (Sprite).
+                pygame.sprite.Sprite.__init__(self)
+
+                self.image = imagens
+                self.rect = self.image.get_rect()
+                self.rect.centerx = WIDTH / 2
+                self.rect.bottom = HEIGHT - 10
+                self.speedx = 0
+                self.speedy = 0
+                self.keys = keys
+
+            def update(self):
+                # Atualização da posição do jogador
+                keys = pygame.key.get_pressed()
+                if keys[self.keys['up']]:
+                    self.speedy = -3
+                    self.speedx = 0
+                elif keys[self.keys['down']]:
+                    self.speedy = 3
+                    self.speedx = 0
+                else:
+                    self.speedy = 0
+
+                if keys[self.keys['left']]:
+                    self.speedx = -3
+                    self.speedy = 0
+                elif keys[self.keys['right']]:
+                    self.speedx = 3
+                    self.speedy = 0
+                else:
+                    self.speedx = 0
+
+                self.rect.x += self.speedx
+                self.rect.y += self.speedy
+
+                # Mantém dentro da tela
+                if self.rect.right > WIDTH:
+                    self.rect.right = WIDTH
+                if self.rect.left < 0:
+                    self.rect.left = 0
+                if self.rect.bottom > HEIGHT:
+                    self.rect.bottom = HEIGHT
+                if self.rect.top < 0:
+                    self.rect.top = 0
+
+        class Peixe(pygame.sprite.Sprite):
+            def __init__(self, imgagens):
+                # Construtor da classe mãe (Sprite).
+                pygame.sprite.Sprite.__init__(self)
+
+                self.image = imgagens
+                self.rect = self.image.get_rect()
+                self.rect.x = 0
+                self.rect.y = random.randint(-100, -altura)
+                self.speedx = 2
+                self.speedy = 0
+
+            def update(self):
+                # Atualizando a posição do peixe
+                self.rect.x += self.speedx
+                self.rect.y += self.speedy
+
+                # Se o peixe passar do final da tela, volta para cima e sorteia
+                # novas posições e velocidades
+                if self.rect.top < 0 or self.rect.bottom > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+                    self.rect.x = 0
+                    self.rect.y = random.randint(-100, HEIGHT - altura)
+                    self.speedx = random.randint(2, 4)
+                    self.speedy = 0
+
+        # ----- Criação de objetos
+        player1 = Player(player_image1, {'up': pygame.K_w, 'down': pygame.K_s, 'left': pygame.K_a, 'right': pygame.K_d})
+        player2 = Player(player_image2, {'up': pygame.K_UP, 'down': pygame.K_DOWN, 'left': pygame.K_LEFT, 'right': pygame.K_RIGHT})
+
+        all_tubaroes = pygame.sprite.Group()
+        all_aguas_vivas = pygame.sprite.Group()
+        all_sprites = pygame.sprite.Group()
+
+        # Adiciona jogadores ao grupo de sprites
+        all_sprites.add(player1)
+        player1.rect.centerx = WIDTH / 3
+        player1.rect.bottom = HEIGHT - 8
+
+        all_sprites.add(player2)
+        player2.rect.centerx = WIDTH / 2
+        player2.rect.bottom = HEIGHT - 12
+
+        for i in range(6):
+            agua_viva1 = Peixe(agua_viva_small)
+            all_aguas_vivas.add(agua_viva1)
+
+        for i in range(2):
+            tubarao = Peixe(tubarao_grande)
+            all_tubaroes.add(tubarao)
+
+        clock = pygame.time.Clock()
+        FPS = 60
+
+        # ===== Loop principal =====
+        while game:
+            clock.tick(FPS)
+            
+            # ----- Trata eventos
+            for event in pygame.event.get():
+                # ----- Verifica consequências
+                if event.type == pygame.QUIT:
+                    game = False
+
+            # ----- Atualiza estado do jogo
+            all_sprites.update()
+
+            # Verifica colisão entre os jogadores e os obstáculos
+            if pygame.sprite.spritecollide(player1, all_tubaroes, False):
+                player1.rect.centerx = WIDTH / 3
+                player1.rect.bottom = HEIGHT - 8
+                score1 = 0
+
+            if pygame.sprite.spritecollide(player2, all_tubaroes, False):
+                player2.rect.centerx = WIDTH / 2
+                player2.rect.bottom = HEIGHT - 15
+                score2 = 0
+            
+            if pygame.sprite.spritecollide(player1, all_aguas_vivas, True):
+                score1 += 1
+                agua_viva1 = Peixe(agua_viva_small)
+                all_aguas_vivas.add(agua_viva1)
+
+            if pygame.sprite.spritecollide(player2, all_aguas_vivas, True):
+                score2 += 1
+                agua_viva1 = Peixe(agua_viva_small)
+                all_aguas_vivas.add(agua_viva1)
+
+            for tubarao in all_tubaroes:
+                tubarao.update()
+            
+            for aguaviva in all_aguas_vivas:
+                aguaviva.update()
+
+            # ----- Gera saídas
+            window.fill((0, 0, 0))  # Preenche com a cor preta
+            window.blit(background, (0, 0))
+
+            all_tubaroes.draw(window)
+            all_aguas_vivas.draw(window)
+            all_sprites.draw(window)
+
+            # Exibe a pontuação na tela
+            score1_text = font.render("Jogador 1: " + str(score1), True, (255, 255, 255))
+            window.blit(score1_text, (10, 10))
+            score2_text = font.render("Jogador 2: " + str(score2), True, (255, 255, 255))
+            window.blit(score2_text, (250, 10))
+
+            pygame.display.update()
+
+        # ===== Finalização =====
+        pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+    pygame.display.update()
+
+# Encerrando o Pygame
+pygame.quit()
